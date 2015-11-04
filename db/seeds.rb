a4d6dd38e4a6ec168cae4ab540cba3c68319a5af
kellen kolbeck & brigham thompson <student@epicodus.com>
2015-11-04 11:42:25 -0800

    add models for plan and subscription. add bootstrap. add index page for plans

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Plan.create(name: 'Quick Fix', price: '$5', time: '7 minutes')
+Plan.create(name: 'The Full Meow', price: '$20', time: '31 minutes')
+Plan.create(name: 'The Full Meow, includes min. of one "Sandpaper Kiss"', price: '$50', time: '32 minutes')
+Plan.create(name: 'Jack Bauer Special', price: '$100', time: '24 hours')
